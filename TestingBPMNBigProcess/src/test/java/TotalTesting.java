870040d5616744853417598bd65fc0c2b7364842
laurenzhinterholzer22 <laurenz.georg@gmail.com>
2022-02-17 11:13:59 +0100

    added first Test

--- TestingBPMNBigProcess/src/test/java/TotalTesting.java
+++ TestingBPMNBigProcess/src/test/java/TotalTesting.java
@@ -1,5 +1,6 @@
 
 
+import generated.process_0a1uc4m.TC_Big_Process_1;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -9,6 +10,12 @@ import java.util.concurrent.Executor;
 public class TotalTesting {
 
 //------------------TASKS----------------------------
+
+    @Rule
+    public TC_Big_Process_1 tc01 = new TC_Big_Process_1();
+    @Test
+    public void tc01AufgabeBenutzer() { tc01.createExecutor().execute(); }
+
 
 
 
